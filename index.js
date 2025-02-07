b152c1a37fb5f6e8cfc0e136c5e43826816c6aae
usn757 <usn757@gmail.com>
2025-02-07 22:29:57 +0900

    first

---
+++ index.js
@@ -0,0 +1,38 @@
+async function makeIssue() {
+  const token = process.env.GH_TOKEN;
+  const OWNER = "TestSetupMyOrganizationByUSN"; // GitHub 계정 이름
+  const REPO = ""; // GitHub 저장소 이름
+
+  // 1~45의 숫자 중 6개 랜덤 생성
+  function generateNumbers() {
+    const numbers = new Set();
+    while (numbers.size < 6) {
+      numbers.add(Math.floor(Math.random() * 45) + 1);
+    }
+    return Array.from(numbers).join(", ");
+  }
+
+  const Numbers = generateNumbers();
+
+  const response = await fetch(
+    `https://api.github.com/repos/${OWNER}/${REPO}/issues`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        title: "이번 주 번호 추천",
+        body: `이번 주 추천 번호는 ${Numbers} 입니다.`,
+      }),
+    }
+  );
+
+  if (response.ok) {
+    console.log("성공");
+  } else {
+    console.log("실패");
+  }
+}
+
+makeIssue();
